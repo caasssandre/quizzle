27efb0a8a12fe4e00c98be836580c9f685012670
student <student@eda-bc-04.localdomain>
2019-12-11 12:20:34 +1300

    Add jumble & api function testing

--- tests/server/routes/questions.test.js
+++ tests/server/routes/questions.test.js
@@ -1,16 +1,36 @@
 const app = require('../../../server/server')
 const request = require('supertest')
 require("babel-polyfill")
+const q = require('../../../server/routes/questions.js')
+const nock = require('nock')
 
-test('it works', () => {
+it('Should works', () => {
   expect(17).toBeGreaterThan(6)
 })
 
+it('Should jumble the array the api calls', () => {
+  const expected = [1,2,3,4]
 
-// describe('get Questions', () => {
-//   it('should get 2 arrays of 5 questions', async () => {
-//     const res = await request(app)
-//       .get('https://opentdb.com/api.php?amount=5&type=multiple')
-//     expect(res.statusCode).toEqual(200)
-//   })
-// })
+  const arr = [1,2,3,4]
+  const actual = q.jumbleQuestions(arr)
+    expect(actual).not.toBe(expected)
+})
+
+
+it('should call an api', () => { 
+  nock('https://opentdb.com')
+  .get('/api.php?amount=3&type=multiple')
+  .reply(200, {results: [ { question: 'What is the title of song on the main menu in Halo?',
+        correct_answer: 'Halo',
+        incorrect_answers: [ 'Opening Suite',
+         'Shadows',
+         'Suite Autumn'] } ]})
+
+ return q.getQuestions(3)
+  .then((actual) => {
+    expect(actual.jumbledTrivias).toHaveLength(1)
+    console.log(actual.jumbledTrivias[0])
+    expect(actual.jumbledTrivias[0]).toHaveProperty('correctAnswer', 'Halo')
+  })
+})
+
